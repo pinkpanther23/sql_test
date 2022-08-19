6b044c1bc56bfdfd33e97a789ecb5e85004cc144
pinkpanther23 <sklimek23@gmail.com>
2022-08-19 11:02:00 -0400

    Add files via upload

--- tk-test.py
+++ tk-test.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+import sqlite3
+
+con = sqlite3.connect('test.db')
+cur = con.cursor()
 
 window = tk.Tk()
 
@@ -46,11 +50,53 @@ def listbox_samples():
 
 s_result = tk.StringVar()
 
-def printResult():
-    data = result.get()
+def createSelect(len, table):
+    if(len == 1):
+        sql_select = "SELECT ? FROM " + table + ";"
+    elif (len == 2):
+        sql_select = "SELECT ?, ? FROM " + table + ";"
+    elif (len == 3):
+        sql_select = "SELECT ?, ?, ? FROM " + table + ";"
+    elif (len == 4):
+        sql_select = "SELECT ?, ?, ?, ? FROM " + table + ";"
+    elif (len == 5):
+        sql_select = "SELECT ?, ?, ?, ?, ? FROM " + table + ";"
+    else:
+        sql_select = "SELECT ? FROM " + table + ";"
+    return sql_select
+
+def convertSelectionS(my_list):
+    new_list = []
+    for i in my_list:
+        if(i == "CD #"):
+            new_list.append("CD_num")
+        elif(i == "Mutation Status"):
+            new_list.append("Mutation_status")
+        elif (i == "Run Number"):
+            new_list.append("Run_num")
+        elif (i == "PG #"):
+            new_list.append("PG_num")
+        elif (i == "DNA #"):
+            new_list.append("DNA_num")
+
+    return new_list
+
+def print_S_Result():
+    data = s_result.get()
     list_data = convertToList(data)
-    for i in list_data:
+    list_size = len(list_data)
+    new_list = convertSelectionS(list_data)
+    combined = ', '.join(new_list)
+    #sql_select = createSelect(list_size, "Samples")
+    sql_select = "SELECT " + combined + " FROM Samples"
+    print(sql_select)
+    cur.execute(sql_select)
+    #cur.execute(sql_select)
+    result = cur.fetchall()
+    for i in result:
         print(i)
+    #for i in list_data:
+        #print(i)
 
 s_btn = tk.Button(window, text = "Submit", fg = "blue", command = listbox_samples)
 s_btn.place(x=40, y=90)
@@ -58,8 +104,8 @@ s_btn.place(x=40, y=90)
 v_btn = tk.Button(window, text = "Submit", fg = "blue")
 v_btn.place(x=200, y=90)
 
-#btn2 = tk.Button(window, text = "Print", fg = "blue", command = printResult)
-#btn2.place(x=90, y=90)
+btn2 = tk.Button(window, text = "Print", fg = "blue", command = print_S_Result)
+btn2.place(x=90, y=90)
 
 #print(result)
 
